2ed2395cfb00d81b56b2460063d1c99f6fbb64f4
valonso <valonsodev@gmail.com>
2022-11-12 02:33:59 +0100

    Quien habría de decirlo que uno debe leer los docs antes de hacer nada

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-
+import * as dotenv from 'dotenv';
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
@@ -16,11 +16,14 @@ import image from "@astrojs/image";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
+
+dotenv.config({ path: `./.env.${process.env.ASTRO_MODE}` });
+
 export default defineConfig({
   server: { host: true },
   integrations: [tailwind(), sitemap(), prefetch(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
   }), vue({ appEntrypoint: '/src/pages/_app' })],
-  site: import.meta.env.SITE_URL,
-  base: import.meta.env.BASE,
+  site: process.env.PUBLIC_SITE_URL,
+  base: process.env.PUBLIC_BASE,
 });
