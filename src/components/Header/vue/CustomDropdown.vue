69a42e0bf2b1dcdc049a4c9132699b4568eec343
valonso <valonsodev@gmail.com>
2022-11-12 02:54:59 +0100

    Nvm

--- src/components/Header/vue/CustomDropdown.vue
+++ src/components/Header/vue/CustomDropdown.vue
@@ -18,6 +18,8 @@ interface Props {
 const props = defineProps<Props>();
 const BASE_URL = import.meta.env.BASE_URL;
 // bg-gradient-to-r bg-fixed from-[#313b41] to-[#46b396]
+console.log(props.currentUrl)
+console.log(`${BASE_URL}${props.page.attributes.slug}`)
 </script>
 <template>
     <Menu
